b199445fbb53933d00cd39ef9697a8112bda95f5
Joseph M <jmedina1985@hotmail.com>
2021-05-23 21:54:17 -0700

    Error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -4,8 +4,11 @@ import { Link } from 'react-router-dom';
 const Error = () => {
   return (
     <Wrapper>
-      <h1>404</h1>
-      <h3>Sorry, the page you tried cannot be found 😿</h3>
+      <div>
+        <h1>404</h1>
+        <h3>Sorry, the page you tried cannot be found 😿</h3>
+        <Link to=""></Link>
+      </div>
     </Wrapper>
   );
 };
