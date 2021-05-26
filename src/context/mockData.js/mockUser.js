8a0e76cc605b9da2816cb65eb0916059a18440a0
Joseph M <jmedina1985@hotmail.com>
2021-05-25 22:13:38 -0700

    Custom user

--- src/context/mockData.js/mockUser.js
+++ src/context/mockData.js/mockUser.js
@@ -6,7 +6,7 @@ export default {
   gravatar_id: '',
   url: 'https://api.github.com/users/jos3ph-m',
   html_url: 'https://github.com/jos3ph-m',
-  followers_url: 'https://api.github.com/users/john-smilga/followers',
+  followers_url: 'https://api.github.com/users/jos3ph-m/followers',
   following_url:
     'https://api.github.com/users/john-smilga/following{/other_user}',
   gists_url: 'https://api.github.com/users/john-smilga/gists{/gist_id}',
