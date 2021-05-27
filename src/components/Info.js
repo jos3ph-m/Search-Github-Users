8967a6148c89bcd66094f7b15396ce990eb98712
Joseph M <jmedina1985@hotmail.com>
2021-05-26 21:55:38 -0700

    Mock data

--- src/components/Info.js
+++ src/components/Info.js
@@ -7,7 +7,7 @@ import { FiUsers, FiUserPlus } from 'react-icons/fi';
 const UserInfo = () => {
   const data = React.useContext(GithubContext);
   console.log(data);
-  return <h2>user component</h2>;
+  return <h2>info component</h2>;
 };
 
 const Wrapper = styled.section`
