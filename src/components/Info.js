d392ba2ce0aed2cf2ef12c1b8c3ab1d1ac52f8d8
Joseph M <jmedina1985@hotmail.com>
2021-05-27 20:42:17 -0700

    Info component

--- src/components/Info.js
+++ src/components/Info.js
@@ -8,7 +8,7 @@ const UserInfo = () => {
   const { githubUser } = React.useContext(GithubContext);
   const { public_repos, followers, following, public_gists } = githubUser;
 
-  const items = [{ id: 1, icon: <GoRepo className="icon" /> }];
+  const items = [{ id: 1, icon: <GoRepo className="icon" />, label: 'repos' }];
 
   return <h2>info component</h2>;
 };
