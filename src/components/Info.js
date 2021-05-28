5572f3ca209d5e2a9584d8e7c25f219174f26118
Joseph M <jmedina1985@hotmail.com>
2021-05-28 14:36:48 -0700

    Info component

--- src/components/Info.js
+++ src/components/Info.js
@@ -39,21 +39,28 @@ const UserInfo = () => {
     },
   ];
 
-  return <section className="section">
-    <Wrapper className="section-center">
-      {items.map(item => return <p>item</p>)}
-    </Wrapper>
-  </section>;
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        {items.map((item) => {
+          return <Item key={item.id} {...item}></Item>;
+        })}
+      </Wrapper>
+    </section>
+  );
 };
 
-const Item = ({icon, label, value, color}) => {
-  return <article className="item">
-    <span className={color}>{icon}</span>
-    <div>
-      <h3></h3>
-    </div>
-  </article>
-}
+const Item = ({ icon, label, value, color }) => {
+  return (
+    <article className="item">
+      <span className={color}>{icon}</span>
+      <div>
+        <h3>{value}</h3>
+        <p>{label}</p>
+      </div>
+    </article>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
