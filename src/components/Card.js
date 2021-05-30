dfbc77c4cdaf4aaca4c9cd72822eed33e7f407d1
Joseph M <jmedina1985@hotmail.com>
2021-05-29 21:25:33 -0700

    Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -20,6 +20,7 @@ const Card = () => {
         <img src={avatar_url} alt={name} />
         <div>
           <h4>{name}</h4>
+          <p>@{twitter_username}</p>
         </div>
       </header>
     </Wrapper>
