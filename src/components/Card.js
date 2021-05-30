0accf73b1ee0577aa4b9c3468f4ff84f6bd0f885
Joseph M <jmedina1985@hotmail.com>
2021-05-29 21:36:58 -0700

    Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -24,7 +24,7 @@ const Card = () => {
         </div>
         <a href={html_url}>follow</a>
       </header>
-      <p className="bio"></p>
+      <p className="bio">bio</p>
     </Wrapper>
   );
 };
