aae56b3d738de97bde5593ba1037e5246902d133
Joseph M <jmedina1985@hotmail.com>
2021-06-01 22:46:25 -0700

    Fusion Charts API

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -5,7 +5,7 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
   console.log(repos);
-  return <section></section>
+  return <section className="section"></section>
   <ExampleChart />;
 };
 
