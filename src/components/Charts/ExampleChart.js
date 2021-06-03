3114f1b5a2261a7a181aa3fb1f69a05ea6efd0ae
Joseph M <jmedina1985@hotmail.com>
2021-06-02 17:44:31 -0700

    Fusion Charts API

--- src/components/Charts/ExampleChart.js
+++ src/components/Charts/ExampleChart.js
@@ -18,32 +18,32 @@ import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
 ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
 
 // STEP 3 - Creating the JSON object to store the chart configurations
-const chartConfigs = {
-  type: 'column2d', // The chart type
-  width: '400', // Width of the chart
-  height: '400', // Height of the chart
-  dataFormat: 'json', // Data type
-  dataSource: {
-    // Chart Configuration
-    chart: {
-      //Set the chart caption
-      caption: 'Countries With Most Oil Reserves [2017-18]',
-      //Set the chart subcaption
-      subCaption: 'In MMbbl = One Million barrels',
-      //Set the x-axis name
-      xAxisName: 'Country',
-      //Set the y-axis name
-      yAxisName: 'Reserves (MMbbl)',
-      numberSuffix: 'K',
-      //Set the theme for your chart
-      theme: 'fusion',
-    },
-    // Chart Data
-    data: chartData,
-  },
-};
 
 const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: 'column2d', // The chart type
+    width: '400', // Width of the chart
+    height: '400', // Height of the chart
+    dataFormat: 'json', // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        //Set the chart caption
+        caption: 'Countries With Most Oil Reserves [2017-18]',
+        //Set the chart subcaption
+        subCaption: 'In MMbbl = One Million barrels',
+        //Set the x-axis name
+        xAxisName: 'Country',
+        //Set the y-axis name
+        yAxisName: 'Reserves (MMbbl)',
+        numberSuffix: 'K',
+        //Set the theme for your chart
+        theme: 'fusion',
+      },
+      // Chart Data
+      data,
+    },
+  };
   return <ReactFC {...chartConfigs} />;
 };
 
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -5,10 +5,25 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
   console.log(repos);
+  const chartData = [
+    {
+      label: 'HTML',
+      value: '13',
+    },
+    {
+      label: 'CSS',
+      value: '23',
+    },
+    {
+      label: 'Javascript',
+      value: '80',
+    },
+  ];
+
   return (
     <section className="section">
       <Wrapper className="section-center">
-        <ExampleChart />;
+        <ExampleChart data={chartData} />;
       </Wrapper>
     </section>
   );
