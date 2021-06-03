f5449a5236b906f0821225d1b94d3ff9e23f08b2
Joseph M <jmedina1985@hotmail.com>
2021-06-02 21:52:06 -0700

    Fusion Charts API

--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -1,19 +1,3 @@
-// STEP 1 - Include Dependencies
-// Include react
-import React from 'react';
-
-// Include the react-fusioncharts component
-import ReactFC from 'react-fusioncharts';
-
-// Include the fusioncharts library
-import FusionCharts from 'fusioncharts';
-
-// Include the chart type
-import Chart from 'fusioncharts/fusioncharts.charts';
-
-// Include the theme as fusion
-import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
-
 // Adding the chart and theme as dependency to the core fusioncharts
 ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
 
