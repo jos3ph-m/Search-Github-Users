7b8e8c1a7a8c44dbe8fefa693a8c829e7f96528c
Joseph M <jmedina1985@hotmail.com>
2021-06-02 17:37:00 -0700

    Fusion Charts API

--- src/components/Charts/ExampleChart.js
+++ src/components/Charts/ExampleChart.js
@@ -36,7 +36,7 @@ const chartData = [
 // STEP 3 - Creating the JSON object to store the chart configurations
 const chartConfigs = {
   type: 'column2d', // The chart type
-  width: '700', // Width of the chart
+  width: '400', // Width of the chart
   height: '400', // Height of the chart
   dataFormat: 'json', // Data type
   dataSource: {
