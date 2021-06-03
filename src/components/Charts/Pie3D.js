00b6e418627d470e9fa8a31f693b92865885dacf
Joseph M <jmedina1985@hotmail.com>
2021-06-02 21:52:44 -0700

    Fusion Charts API

--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -5,7 +5,7 @@ ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
 
 const ChartComponent = ({ data }) => {
   const chartConfigs = {
-    type: 'pie2d', // The chart type
+    type: 'pie3d', // The chart type
     width: '400', // Width of the chart
     height: '400', // Height of the chart
     dataFormat: 'json', // Data type
