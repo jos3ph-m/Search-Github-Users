23f82e8a38dc7744693ad8ab1f21483b394287d5
Joseph M <jmedina1985@hotmail.com>
2021-06-03 21:51:12 -0700

    Fusion Charts API

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -5,7 +5,10 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
   console.log(repos);
-  let languages = repos.reduce((total, item) => {}, {});
+  let languages = repos.reduce((total, item) => {
+    console.log(item);
+    return total;
+  }, {});
   const chartData = [
     {
       label: 'HTML',
