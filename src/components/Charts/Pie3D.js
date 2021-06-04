73731d77d2737849ddcd08a1c2fcc465e38cf0c0
Joseph M <jmedina1985@hotmail.com>
2021-06-03 21:35:04 -0700

    Fusion Charts API

--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -30,7 +30,7 @@ const ChartComponent = ({ data }) => {
         theme: 'fusion',
         decimals: 0,
         pieRadius: '45%',
-        paletteColors:
+        paletteColors: '#f0db4f',
       },
       // Chart Data
       data,
