a8d855b639692e79836c89ca627e2548c396fe84
Joseph M <jmedina1985@hotmail.com>
2021-06-04 21:52:09 -0700

    Calculate most used language

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -19,7 +19,9 @@ const Repos = () => {
 
     return total;
   }, {});
-  languages = Object.values(languages).sort(() => {});
+  languages = Object.values(languages).sort((a, b) => {
+    return b.value - a.value;
+  });
   const chartData = [
     {
       label: 'HTML',
