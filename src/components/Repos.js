64be26116bc6d19a4d1c042442691b00ade5928b
Joseph M <jmedina1985@hotmail.com>
2021-06-05 21:59:06 -0700

    fusion charts - responsive

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -43,7 +43,8 @@ const Repos = () => {
     <section className="section">
       <Wrapper className="section-center">
         <Pie3D data={languages}></Pie3D>
-        <ExampleChart data={chartData} />;
+        <div></div>
+        <ExampleChart data={chartData} />;<div></div>
       </Wrapper>
     </section>
   );
