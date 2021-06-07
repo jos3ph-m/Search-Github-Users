e1285de25f2f324d521f794485253410ba2079a7
Joseph M <jmedina1985@hotmail.com>
2021-06-06 21:46:45 -0700

    Calculate most popular language

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -20,7 +20,7 @@ const Repos = () => {
 
     return total;
   }, {});
-  languages = Object.values(languages)
+  const mostUsed = Object.values(languages)
     .sort((a, b) => {
       return b.value - a.value;
     })
