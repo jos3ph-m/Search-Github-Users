742c968134324461688a710bbbc06987d5ef0b45
Joseph M <jmedina1985@hotmail.com>
2021-06-11 22:01:10 -0700

    Requests

--- src/components/Search.js
+++ src/components/Search.js
@@ -28,7 +28,7 @@ const Search = () => {
               value={user}
               onChange={(e) => setUser(e.target.value)}
             />
-            <button type="submit">search</button>
+            {requests > 0 && <button type="submit">search</button>}
           </div>
         </form>
         <h3> requests: {requests} / 60</h3>
