ae4d43bd1f49e90ed336b34dd5decb813275dcc3
Joseph M <jmedina1985@hotmail.com>
2021-06-12 23:09:07 -0700

    Search user

--- src/context/context.js
+++ src/context/context.js
@@ -26,6 +26,7 @@ const GithubProvider = ({ children }) => {
     const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
       console.log(err)
     );
+    console.log(response);
     if (response) {
       setGithubUser(response.data);
     }
