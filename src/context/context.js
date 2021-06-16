a0dbe6dff5d369f5d3a4a960a6ba63ea16ed9e0f
Joseph M <jmedina1985@hotmail.com>
2021-06-15 21:55:00 -0700

    Promise.allSettled()

--- src/context/context.js
+++ src/context/context.js
@@ -34,7 +34,9 @@ const GithubProvider = ({ children }) => {
       await Promise.allSettled([
         axios(`${rootUrl}/users/${login}/repos?per_page=100`),
         axios(`${followers_url}?per_page=100`),
-      ]);
+      ]).then((results) => {
+        console.log(results);
+      });
     } else {
       toggleError(true, 'there is no user with that username');
     }
