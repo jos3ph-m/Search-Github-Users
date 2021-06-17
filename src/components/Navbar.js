d4887b979879df1f181316cc2d95e61ef1ad8220
Joseph M <jmedina1985@hotmail.com>
2021-06-16 21:27:52 -0700

    Auth0 setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,11 @@ import { useAuth0 } from '@auth0/auth0-react';
 const Navbar = () => {
   const { isAuthenticated, loginWithRedirect, logout, user, isLoading } =
     useAuth0();
-  return <Wrapper>navbar component</Wrapper>;
+  return (
+    <Wrapper>
+      <button></button>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.nav`
