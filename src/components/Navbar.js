555c75d06e72712c1afbc34d737e7575bb85d82c
Joseph M <jmedina1985@hotmail.com>
2021-06-17 15:25:42 -0700

    login logout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 const Navbar = () => {
   const { isAuthenticated, loginWithRedirect, logout, user, isLoading } =
     useAuth0();
+  const isUser = isAuthenticated && user;
   console.log({ isAuthenticated, user, isLoading });
   return (
     <Wrapper>
