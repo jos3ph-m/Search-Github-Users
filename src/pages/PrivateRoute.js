fa8600ed3a7ae678b75be6ee7a0be4f828711c8d
Joseph M <jmedina1985@hotmail.com>
2021-06-17 21:51:37 -0700

    Private route

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -4,6 +4,15 @@ import { useAuth0 } from '@auth0/auth0-react';
 
 const PrivateRoute = ({ children, ...rest }) => {
   const isUser = true;
-  return <Route {...rest}>private route component</Route>;
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        return isUser ? children : <Redirect to="/login" />;
+      }}
+    >
+      private route component
+    </Route>
+  );
 };
 export default PrivateRoute;
