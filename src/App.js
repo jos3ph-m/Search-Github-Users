7bb2a5a6482a7ca55638bcf6c54973ad734eae24
Joseph M <jmedina1985@hotmail.com>
2021-06-18 18:08:57 -0700

    Private route

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <PrivateRoute path="/" exact={true}>
-          <Dashboard></Dashboard>
-        </PrivateRoute>
-        <Route path="/login" exact={true}>
-          <Login />
-        </Route>
-        <Route path="*">
-          <Error></Error>
-        </Route>
-      </Switch>
-    </Router>
+    <AuthWrapper>
+      <Router>
+        <Switch>
+          <PrivateRoute path="/" exact={true}>
+            <Dashboard></Dashboard>
+          </PrivateRoute>
+          <Route path="/login" exact={true}>
+            <Login />
+          </Route>
+          <Route path="*">
+            <Error></Error>
+          </Route>
+        </Switch>
+      </Router>
+    </AuthWrapper>
   );
 }
 
