7e36750746c6f8b8c0fd91d04506a342ea1c3255
Joseph M <jmedina1985@hotmail.com>
2021-06-18 18:12:16 -0700

    Wrapper

--- src/pages/AuthWrapper.js
+++ src/pages/AuthWrapper.js
@@ -4,7 +4,7 @@ import loadingGif from '../images/preloader.gif';
 import styled from 'styled-components';
 function AuthWrapper({ children }) {
   const { isLoading, error } = useAuth0();
-  if (loading) {
+  if (isLoading) {
     return (
       <Wrapper>
         <img src={loadingGif} alt="spinner" />
