2e35fc040e0f444522ff425f5212a40dc950b4bb
Joseph M <jmedina1985@hotmail.com>
2021-06-18 17:56:48 -0700

    Private route

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -3,7 +3,8 @@ import { Route, Redirect } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const PrivateRoute = ({ children, ...rest }) => {
-  const isUser = false;
+  const { isAuthenticated, user } = useAuth0();
+  const isUser = isAuthenticated && user;
   return (
     <Route
       {...rest}
